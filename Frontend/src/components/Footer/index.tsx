22ee8f3565fec5f1384f51775c0253fda7b2335a
Caio_Henrique <caio@victsoft.com.br>
2022-07-15 15:27:19 -0300

    Implementation of all screen components

---
+++ Frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="dsmeta-logo-container">
+            <p>&copy; Desenvolvido por <a href="https://www.instagram.com/caiohenrique3/" target="_blank">@caiohenrique3</a></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
